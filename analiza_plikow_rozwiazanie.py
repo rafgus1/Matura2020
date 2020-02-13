6d000d4ae4e73128c52ea6665fdb2447498fdd28
rafgus1 <53905819+rafgus1@users.noreply.github.com>
2020-02-13 19:08:26 +0100

    zadania 1, 2, 3

---
+++ analiza_plikow_rozwiazanie.py
@@ -0,0 +1,71 @@
+plik = open('dane_obrazki.txt ')
+obrazy = []
+obraz = []
+for linia in plik:
+    if len(linia.strip())>0:
+        obraz.append(linia.strip())
+    else:
+        obrazy.append(obraz)
+        obraz=[]
+#zadanie 1
+rewers = 0
+for obraz in obrazy:
+    ileJedynek=0
+    ileZer = 0
+    for linia in obraz[0:20]:
+        ileJedynek+=linia[0:-1].count('1')
+    if ileJedynek>200:
+        rewers+=1
+print(rewers)
+#zadanie 2
+rekurencyjne=0
+pierwszy = True
+for obraz in obrazy:
+    c1,c2,c3,c4=[],[],[],[]
+    for linia in obraz[0:10]:
+        c1.append(linia[0:10])
+        c2.append(linia[10:20])
+    for linia in obraz[10:20]:
+        c3.append(linia[0:10])
+        c4.append(linia[10:20])
+    if c1==c2==c3==c4:
+        rekurencyjne+=1
+        if pierwszy:
+            for wiersz in obraz[0:20]:
+                print(wiersz[0:20])
+            pierwszy=False
+print(rekurencyjne)
+#zadanie3
+poprawne=0
+nienaprawialne=0
+naprawialne = 0
+for obraz in obrazy:
+    pion=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    bladW=0
+    bladK=0
+    poprawneWiersze=True
+    poprawneKolumny=True
+    for linia in obraz[0:20]:
+        if linia[0:20].count('1')%2==int(linia[-1]):
+            pass
+        else:
+            bladW+=1
+            poprawneWiersze=False
+        for i in range(0,20):
+            if linia[i]=='1':
+                pion[i]+=1
+    ostatni=obraz[-1]
+    for i in range(0,20):
+        if pion[i]%2==int(ostatni[i]):
+            pass
+        else:
+            bladK+=1
+            poprawneKolumny=False
+    if poprawneKolumny and poprawneWiersze:
+        poprawne+=1
+    elif bladW>1 and bladK>1 or bladW>1 or bladK>1:
+            nienaprawialne+=1
+naprawialne = 200-poprawne-nienaprawialne
+print(poprawne, naprawialne, nienaprawialne)
+
+    
